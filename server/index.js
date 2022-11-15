c37b735d175273be0244585eb67f9b28ec92c4b9
haseeb-ali-dev <h45.busy@gmail.com>
2022-11-15 10:13:07 -0800

    material ui and auth wip

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const User = require('./models/user')
+const sequelize = require('./database')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+
+console.log(User === sequelize.models.User)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
