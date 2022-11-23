5a3640939a30cb405deb28213a8fc1f3943dbe00
Haseeb Ali <haseeb.rehmat@devsinc.com>
2022-11-23 10:42:20 -0800

    protected and authenticated

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { Home, Signin, Signup } from 'containers'
+import Protected from './protected'
+import Authenticated from './authenticated'
 
 const Router = () => {
   const [user, setUser] = useState({ email: '', password: '' })
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/create-account' element={<Signup user={user} setUser={setUser} />} />
-      <Route path='/login' element={<Signin user={user} setUser={setUser} />} />
+      <Route path='/' element={<Protected><Home /></Protected>} />
+      <Route path='/create-account' element={<Authenticated><Signup user={user} setUser={setUser} /></Authenticated>} />
+      <Route path='/login' element={<Authenticated><Signin user={user} setUser={setUser} /></Authenticated>} />
       <Route path='*' element={<Navigate to='/' />} />
     </Routes>
   )
