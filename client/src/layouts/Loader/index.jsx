4f5e5db45becf43a753366c25fd1ceca411f0f34
haseeb-ali-dev <h45.busy@gmail.com>
2022-11-24 05:34:17 -0800

    loader overlay

--- client/src/layouts/Loader/index.jsx
+++ client/src/layouts/Loader/index.jsx
@@ -1,14 +1,19 @@
-import { Box, CircularProgress } from '@mui/material'
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
 
-export default function CircularIndeterminate() {
+const Loader = () => {
+  const open = true
+
   return (
-    <Box
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
-      minHeight='100vh'
-    >
-      <CircularProgress color='warning' />
-    </Box>
+    <div>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      >
+        <CircularProgress color='warning' />
+      </Backdrop>
+    </div>
   )
 }
+
+export default Loader
