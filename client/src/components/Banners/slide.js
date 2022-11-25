82f3397503b6cee47dced94edbf2edf290e3dee5
Haseeb Ali <haseeb.rehmat@devsinc.com>
2022-11-25 11:19:50 -0800

    banners and navbar items hide in sm

--- client/src/components/Banners/slide.js
+++ client/src/components/Banners/slide.js
@@ -1,15 +1,23 @@
 import { Box, Button, Typography } from '@mui/material'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import { ChatBubbleOutline, ArrowBack, ArrowForward } from '@mui/icons-material'
 
 const Slide = ({
-  next, prev, text,
+  next, prev, text = 'Slider', btnText = 'Click', head = 'Heading', featured = 'Featured Talent',
 }) => (
   <Box className='slider-content'>
+    <Typography textTransform='uppercase' color='#f8b500' fontWeight='bold' fontSize='16px'>{featured}</Typography>
+    <Typography fontSize='40px'>{head}</Typography>
     <Typography>{text}</Typography>
+    <Button
+      id='theme-btn'
+      sx={{
+        mt: 5, mb: 2, fontSize: '1.25rem', px: 2, fontWeight: 'bold', pt: 1, borderRadius: 2,
+      }}
+      startIcon={<ChatBubbleOutline fontSize='large' />}
+    >{btnText}</Button>
     <Box className='slider-navigate'>
-      <Button id='theme-btn2' onClick={prev}><ArrowBackIcon fontSize='large' /></Button>
-      <Button id='theme-btn2' onClick={next}><ArrowForwardIcon fontSize='large' /></Button>
+      <Button id='theme-btn2' onClick={prev}><ArrowBack fontSize='large' /></Button>
+      <Button id='theme-btn2' onClick={next}><ArrowForward fontSize='large' /></Button>
     </Box>
     <Box className='swiper-pagination' />
   </Box>
