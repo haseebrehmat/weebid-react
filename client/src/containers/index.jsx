25ebd792a1a3eb2b5106584697fedb30c0b05406
haseeb-ali-dev <h45.busy@gmail.com>
2022-11-29 03:07:53 -0800

    profile page wip

--- client/src/containers/index.jsx
+++ client/src/containers/index.jsx
@@ -1,5 +1,6 @@
 import { lazy } from 'react'
 
 export const Home = lazy(() => import('containers/home'))
+export const Profile = lazy(() => import('containers/profile'))
 export const Signup = lazy(() => import('containers/signup'))
 export const Signin = lazy(() => import('containers/signin'))
