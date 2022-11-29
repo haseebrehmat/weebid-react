66914084eb7919a9c4b2596331fec0f8a2dcc10d
Haseeb Ali <haseeb.rehmat@devsinc.com>
2022-11-29 05:45:42 -0800

    profile image and make pitch btn

---
+++ client/src/components/Profile/Image/index.jsx
@@ -0,0 +1,32 @@
+import {
+  CardMedia, Card, Box, Typography, Button, IconButton,
+} from '@mui/material'
+
+import {
+  makePitchProps, threeDotsProps,
+  nameAndActionsProps, imageProps, profileImgCard,
+} from './props'
+
+import { MoreHoriz } from '@mui/icons-material'
+import image from 'assets/slider5.png'
+
+const ProfileImage = () => (
+  <Card {...profileImgCard}>
+    <Box style={{ position: 'relative' }}>
+      <CardMedia {...imageProps(image)} />
+      <Box {...nameAndActionsProps}>
+        <Typography variant='inherit' color='white' ml='10px'>
+          Haseeb Rehmat ALi
+        </Typography>
+        <IconButton {...threeDotsProps}>
+          <MoreHoriz fontSize='small' />
+        </IconButton>
+      </Box>
+    </Box>
+    <Button {...makePitchProps}>
+      Make a pitch to Haseeb Rehmat ALi
+    </Button>
+  </Card>
+)
+
+export default ProfileImage
