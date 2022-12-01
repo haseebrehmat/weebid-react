371f319f36cb191b573a4171db2aa856508636ff
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-01 15:22:54 -0800

    question page wip

--- client/src/components/Pitches/details.js
+++ client/src/components/Pitches/details.js
@@ -1,10 +1,13 @@
-import { CardContent, Typography } from '@mui/material'
+import { CardContent, Typography, Box } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const PitchDetails = () => (
   <CardContent>
-    <Typography gutterBottom variant='h5' component='div' color='white'>
-      Pitch
-    </Typography>
+    <Box component={Link} to='/question/10' sx={{ color: 'white', textDecoration: 'none' }}>
+      <Typography gutterBottom variant='h5'>
+        Pitch
+      </Typography>
+    </Box>
     <Typography variant='body1' color='#8e8d8d'>
       $6 Raised | 2 Pledges
     </Typography>
