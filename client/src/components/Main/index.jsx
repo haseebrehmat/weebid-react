bf453eb138a533c28c2932ccb7668ae92a2e7bb5
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-01 08:46:12 -0800

    stashed code recovered

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import {
-  Home, Signin, Signup, Profile, Ask,
+  Home, Signin, Signup, Profile, Ask, Explore,
 } from 'containers'
 
 import Protected from './protected'
@@ -15,6 +15,7 @@ const Main = () => {
     <Routes>
       <Route path='/' element={<Protected><Home /></Protected>} />
       <Route path='/profile' element={<Protected><Profile /></Protected>} />
+      <Route path='/search' element={<Protected><Explore /></Protected>} />
       <Route path='/ask/user/:id' element={<Protected><Ask /></Protected>} />
       <Route path='/create-account' element={<Authenticated><Signup user={user} setUser={setUser} /></Authenticated>} />
       <Route path='/login' element={<Authenticated><Signin user={user} setUser={setUser} /></Authenticated>} />
