01fa33e43698fef8ff6ed82f371b84bc26ca3e6f
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-01 08:59:24 -0800

    homepage pitches

--- client/src/containers/home/index.jsx
+++ client/src/containers/home/index.jsx
@@ -1,7 +1,13 @@
-import { Banners } from 'components'
+import { Box, Stack } from '@mui/material'
+import { Banners, ProfilePitches } from 'components'
 
 const Home = () => (
-  <Banners />
+  <Stack spacing={2}>
+    <Banners />
+    <Box px={10}>
+      <ProfilePitches />
+    </Box>
+  </Stack>
 )
 
 export default Home
