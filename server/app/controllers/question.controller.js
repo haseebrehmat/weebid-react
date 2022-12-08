5040f1ef0d3de8aa70699d7f4e244cec10a3f490
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-08 07:09:29 -0800

    pagination in home pitches

--- server/app/controllers/question.controller.js
+++ server/app/controllers/question.controller.js
@@ -13,14 +13,28 @@ exports.create = async (req, res) => {
 
 exports.index = async (req, res) => {
   try {
-    const { id } = req.query;
-    var condition = id ? { receiverId: id } : null;
+    const { page } = req.query;
     const questions = await Question.findAll({
       include: ['sender', 'receiver'],
-      where: condition,
+      limit: 8,
+      offset: (page - 1) * 8,
     });
     return res.send(questions);
   } catch (error) {
-    return res.status(500).send({ msg: err.message || "Some error occurred while retrieving users." });
+    return res.status(500).send({ msg: err.message || "Some error occurred while retrieving questions." });
   }
 }
+
+// exports.index = async (req, res) => {
+//   try {
+//     const { id } = req.query;
+//     var condition = id ? { receiverId: id } : null;
+//     const questions = await Question.findAll({
+//       include: ['sender', 'receiver'],
+//       where: condition,
+//     });
+//     return res.send(questions);
+//   } catch (error) {
+//     return res.status(500).send({ msg: err.message || "Some error occurred while retrieving users." });
+//   }
+// }
