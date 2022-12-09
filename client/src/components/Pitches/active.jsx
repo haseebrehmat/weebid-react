1ee7dcbaf2ebb3083265ca18a46086f94c70d9e7
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-09 07:37:46 -0800

    image on error of pitch

--- client/src/components/Pitches/active.jsx
+++ client/src/components/Pitches/active.jsx
@@ -6,7 +6,7 @@ import { pitchImgProps } from './props'
 
 const ActivePitch = ({ pitch }) => (
   <Card sx={{ bgcolor: '#000000c2', mb: 2 }}>
-    <CardMedia {...pitchImgProps(pitch.receiver.avatar, 'Active Pitch')} />
+    <CardMedia {...pitchImgProps(pitch.receiver.avatar, pitch.receiver.name)} />
     <PitchDetails msg={pitch.message} id={pitch.id} />
     <ActivePitchActions />
   </Card>
