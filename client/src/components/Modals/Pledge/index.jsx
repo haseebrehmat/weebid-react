64e545fe170501aba296e41d0c47aea49846b624
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-12 13:08:33 -0800

    pledge modal

--- client/src/components/Modals/Pledge/index.jsx
+++ client/src/components/Modals/Pledge/index.jsx
@@ -1,14 +1,38 @@
-import { memo } from 'react'
-import { Box } from '@mui/material'
+import { memo, useState } from 'react'
+import {
+  OutlinedInput, FormControl,
+} from '@mui/material'
 
 import { BaseModal } from 'components'
 
-const PledgeModal = ({ title = 'Make a Pledge' }) => (
-  <BaseModal title={title}>
-    <Box>
-      Hello World
-    </Box>
-  </BaseModal>
-)
+const PledgeModal = ({ title = 'Make a Pledge', clearShow = null }) => {
+  const [amount, setAmount] = useState(0)
+
+  const handleClick = () => {
+    if (amount) setAmount(120)
+  }
+
+  return (
+    <BaseModal title={title} clearShow={clearShow} handleClick={handleClick}>
+      <FormControl sx={{ width: '98%' }}>
+        <OutlinedInput
+          placeholder='0'
+          sx={{
+            color: '#f3f3f3',
+            m: 'auto',
+            width: 'inherit',
+            fontSize: '30px',
+            border: '1px solid #686868',
+            borderRadius: '10px',
+            height: '60px',
+          }}
+          startAdornment='$'
+          value={amount}
+          onChange={e => setAmount(e.target.value)}
+        />
+      </FormControl>
+    </BaseModal>
+  )
+}
 
 export default memo(PledgeModal)
--- client/src/components/Pitch/Actions/index.jsx
+++ client/src/components/Pitch/Actions/index.jsx
@@ -1,26 +1,33 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { Box, Button } from '@mui/material'
 import { Cancel, Key, MilitaryTech } from '@mui/icons-material'
 
+import { PledgeModal } from 'components'
+
 import { actionBtnProps, makePledgeBtnProps } from './props'
 
-const PitchActions = memo(() => (
-  <>
-    <Box mt={2}>
-      <Button {...actionBtnProps} startIcon={<Cancel />}>
-        Set as Goal
-      </Button>
-      <Button {...actionBtnProps} startIcon={<Key />}>
-        Add Innicative
-      </Button>
-      <Button {...actionBtnProps} startIcon={<MilitaryTech />}>
-        Donate
-      </Button>
-    </Box>
-    <Button {...makePledgeBtnProps}>
-      Make a Pledge
-    </Button>
-  </>
-))
+const PitchActions = () => {
+  const [pledgeModal, setPledgeModal] = useState(false)
 
-export default PitchActions
+  return (
+    <>
+      <Box mt={2}>
+        <Button {...actionBtnProps} startIcon={<Cancel />}>
+          Set as Goal
+        </Button>
+        <Button {...actionBtnProps} startIcon={<Key />}>
+          Add Innicative
+        </Button>
+        <Button {...actionBtnProps} startIcon={<MilitaryTech />}>
+          Donate
+        </Button>
+      </Box>
+      <Button {...makePledgeBtnProps} onClick={() => setPledgeModal(true)}>
+        Make a Pledge
+      </Button>
+      {pledgeModal ? <PledgeModal clearShow={setPledgeModal} /> : null}
+    </>
+  )
+}
+
+export default memo(PitchActions)
