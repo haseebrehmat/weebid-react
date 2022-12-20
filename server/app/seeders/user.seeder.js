dc38d6ed9e9796309cbfed30010d9a0fbfae96c6
haseeb-ali-dev <h45.busy@gmail.com>
2022-12-20 09:57:12 -0800

    seeders revised and finduser with then-catch

--- server/app/seeders/user.seeder.js
+++ server/app/seeders/user.seeder.js
@@ -24,35 +24,3 @@ for (let index = 1; index <= 50; index++) {
 module.exports = () => {
   User.bulkCreate(users);
 }
-
-
-// module.exports = () => {
-//   User.bulkCreate([
-//     {
-//       name: "Super Admin",
-//       email: "admin@test.com",
-//       password: "$2b$08$tD1epQXB6t3hYseRy0ROh.RKUvkg/1bldp1RXvXi8/Hg.apyvR06O",
-//       role: "admin",
-//     },
-//     {
-//       name: "User 1",
-//       email: "user1@test.com",
-//       password: "$2b$08$tD1epQXB6t3hYseRy0ROh.RKUvkg/1bldp1RXvXi8/Hg.apyvR06O",
-//     },
-//     {
-//       name: "User 2",
-//       email: "user2@test.com",
-//       password: "$2b$08$tD1epQXB6t3hYseRy0ROh.RKUvkg/1bldp1RXvXi8/Hg.apyvR06O",
-//     },
-//     {
-//       name: "User 3",
-//       email: "user3@test.com",
-//       password: "$2b$08$tD1epQXB6t3hYseRy0ROh.RKUvkg/1bldp1RXvXi8/Hg.apyvR06O",
-//     },
-//     {
-//       name: "User 4",
-//       email: "user4@test.com",
-//       password: "$2b$08$tD1epQXB6t3hYseRy0ROh.RKUvkg/1bldp1RXvXi8/Hg.apyvR06O",
-//     },
-//   ]);
-// }
