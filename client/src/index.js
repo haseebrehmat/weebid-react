8b25e1d29d413bff9d0b367732fe764c90477779
Haseeb Ali <haseeb.rehmat@devsinc.com>
2022-12-21 09:46:34 -0800

    react query devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import './index.css'
 import App from './App'
 import { queryClient } from 'utils/clients'
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
