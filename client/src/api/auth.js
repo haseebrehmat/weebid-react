8a67fbbc1d1c39f4f7493223e62ddd7aad65f085
Haseeb Ali <haseeb.rehmat@devsinc.com>
2022-12-27 07:07:49 -0800

    env in server and auth messages

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,16 +1,16 @@
 import http from 'utils/http'
-import { saveToken } from 'utils/helpers'
+import { getMsg, saveToken } from 'utils/helpers'
 
 export const signup = (email, password) => http.post('auth/signup', { email, password })
   .then(res => {
     saveToken(res.data.accessToken)
     return { status: 'success', msg: res.data.message }
   })
-  .catch(error => ({ status: 'error', msg: error.response.data.msg }))
+  .catch(error => ({ status: 'error', msg: getMsg(error) }))
 
 export const signin = (email, password) => http.post('auth/signin', { email, password })
   .then(res => {
     saveToken(res.data.accessToken)
     return { status: 'success', msg: res.data.message }
   })
-  .catch(error => ({ status: 'error', msg: error.response.data.msg }))
+  .catch(error => ({ status: 'error', msg: getMsg(error) }))
